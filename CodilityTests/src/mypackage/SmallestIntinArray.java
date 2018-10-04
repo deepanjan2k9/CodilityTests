9e5492eada6c23f7795f9f66ef7d40d8e7e76e6e
Deepanjan Das <deepanjan.2k9@gmail.com>
2018-10-04 14:54:01 -0400

    Finalized solution and added comments

--- CodilityTests/src/mypackage/SmallestIntinArray.java
+++ CodilityTests/src/mypackage/SmallestIntinArray.java
@@ -19,90 +19,74 @@ each element of array A is an integer within the range [−1,000,000..1,000,000]
 
 package mypackage;
 
-import java.util.Arrays;
-import java.lang.Math;
-
 public class SmallestIntinArray {
 	
+	/*
+	 * Utility function to sort the array. I am using
+	 * bubble sort for ease of coding
+	 * */
+	public int[] Sort(int[] a) {
+		boolean swap=false;
+		int[] array = a;
+		int temp;
+		
+		do {
+			swap=false;
+			for(int i=0; i<array.length-1; i++) {
+				if(array[i] > array[i+1]) {
+					temp=array[i+1];
+					array[i+1] = array[i];
+					array[i] = temp;
+					swap = true;
+				}	
+			}
+		}while(swap==true);
+		
+		return array;
+	}
 	
 	public int Solution(int[] A) {
+		//make a copy of array
+		int[] a = A;
 		
-		System.out.println("Array is");
-		for(int x:A) {
-			System.out.print(x + " ");
-		}
-		System.out.println();
+		//sort the array copy in ascending order
+		a = Sort(a);
 		
-		//find the smallest value in array
-		int min = A[0];
-		for(int i=1; i<A.length; i++) {
-			if(min > A[i]) {
-				min = A[i];
+		//ans starts with min value in array
+		int ans = a[0];
+		
+		for(int i = 0; i < a.length; i++) {
+			//we are only concerned with a positive
+			//outcome, so final ans has to be 
+			//positive anyway
+			if(a[i] <= 0) {
+				ans = 0;
+			}
+			if(ans == a[i]) {
+				ans++;
 			}
 		}
 		
-		//find the max value in array
-		int max = A[0];
-		for(int i=1; i<A.length; i++) {
-			if(max < A[i]) {
-				max = A[i];
-			}
+		//to ensure that ans is minimum positive
+		if(ans == 0) {
+			ans++;
 		}
 		
-		int candidate = min + 1;
-		
-		//matchFlag is true when value of candidate matches
-		//a value in the given array. Used to exit the
-		//while loop.
-		boolean matchFlag = false;
-		
-		/*
-		 * The while loop runs multiple passes on the array.
-		 * At each pass, the inner for loop checks if candidate
-		 * exists in the array. If it does, then it increments
-		 * candidate and goes for the next pass. The number of 
-		 * passes is determined by value of candidate - the 
-		 * while loop runs at least till candidate equals max
-		 * value in array. In case of an array like {1,2,3}, the 
-		 * while loop's last loop runs when candidate=3, and the 
-		 * for loop finds this value in the array, and increments
-		 * candidate. So we end up with answer being one larger than
-		 * the max value in the array.
-		 * */
-		while(candidate <= max ) {
-			for(int i=0; i<A.length; i++) {
-				if(candidate == A[i]) {
-					candidate++;
-					matchFlag = true;
-					
-				} else {
-					matchFlag = false;	
-				}
-			}
-			//exit while loop
-			if(!matchFlag) {
-				break;
-			}
-		}
-		
-		//to ensure that candidate is always positive minimum
-		if(candidate <= 0) {
-			candidate = 1;
-		}
-		
-		return candidate;
-		
+		return ans;
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SmallestIntinArray obj = new SmallestIntinArray();
-		//int[] A = {-1,-3};
-		//int[] A = {1,2,3};
-		//int[] A = {1, 3, 6, 4, 1, 2};
-		//int[] A = {-13, -8, 0};
-		//int[] A = {-13, 2, 0};
-		//int[] A = {-13,13,0};
-		int[] A = {-13,1,0};
+		//TEST CASES
+		//int[] A = {-1,-3};	PASS
+		//int[] A = {1,2,3};	PASS
+		//int[] A = {1, 3, 6, 4, 1, 2};	PASS
+		//int[] A = {-13, -8, 0};	PASS
+		//int[] A = {-13, 2, 0};	PASS
+		//int[] A = {-13,13,0};	PASS
+		int[] A = {-13,1,0}; //PASS
+		
 		int ans = obj.Solution(A);
 		System.out.println("Minimum positive integer not in array: " + ans);
 	}
